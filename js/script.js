888fe2aca173e2891ef4d68fa9eaed6a3a6b20ac
FliteFactory <130686782+FliteFactory@users.noreply.github.com>
2023-12-23 21:27:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+
+$(document).ready(function () {
+    $("a[href*='#']").on("click", function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 444);
+        e.preventDefault();
+        return false;
+    });
+});
